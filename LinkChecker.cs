39c4cf07d66ab009d27816d9d0b53a77a7805fb4
Aaron Dinnage <43110368+AaronDinnage@users.noreply.github.com>
2021-02-09 01:35:00 +1100

    Visio Tools
    Link Checker - C# Console App - checks for broken links and can run in update mode to make all links open in a new window and replace using CSV file of link substitutions to make.
    SVG Fixer - C# Console App - removes all the superfluous title, desc, comments, and whitespace from SVG files exported from Visio.
    Visio Exporter - VB App code - run in Visio to locate all VSDX files in the current folder and export PNG, PDF, and SVG files from them based on the page Title of each page in each file.

---
+++ LinkChecker.cs
@@ -0,0 +1,403 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace VisioLinkChecker
+{
+    class Program
+    {
+        public class VisioPage
+        {
+            public string File;
+            public string Page;
+            public string Id;
+            public string DisplayName;
+        }
+
+        public class LinkDetails
+        {
+            public string File;
+            public string Page;
+            public string Description;
+            public string Url;
+            public bool Broken;
+        }
+
+        enum AppMode
+        {
+            None = 0,
+            Check,
+            Update,
+        }
+
+        static AppMode mode = AppMode.None;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Visio Link Checker");
+            Console.WriteLine("by Aaron Dinnage");
+            Console.WriteLine();
+
+            if (args.Length < 2)
+            {
+                Usage();
+                return;
+            }
+
+            var files = ProcessArguments(args);
+            if (mode == AppMode.None || files == null || files.Count == 0)
+            {
+                Usage();
+                return;
+            }
+
+            switch (mode)
+            {
+                case AppMode.Check:
+                    CheckLinks(files);
+                    break;
+
+                case AppMode.Update:
+                    UpdateLinks(files);
+                    break;
+            }
+        }
+
+        static void Usage()
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine("      VisioLinkChecker.exe {check|update} <file_or_folder_paths>...");
+            Console.WriteLine();
+            Console.WriteLine("The first parameter is either 'check' or 'update' to set the mode of the app.");
+            Console.WriteLine("In Update mode a local file named 'LinkUpdates.csv' must be present with a 'find,replace' format inside.");
+            Console.WriteLine("All other parameters will be considered a file or folder path to be processed.");
+            Console.WriteLine("* Files will be individually processed.");
+            Console.WriteLine("* Folders will have all .VSDX files inside them processed (excludes sub-folders).");
+            Console.WriteLine();
+        }
+
+        static List<string> ProcessArguments(string[] args)
+        {
+           switch (args[0].ToLowerInvariant())
+            {
+                case "check":
+                case "c":
+                    mode = AppMode.Check;
+                    break;
+                
+                case "update":
+                case "u":
+                    mode = AppMode.Update;
+                    break;
+                
+                default:
+                    mode = AppMode.None;
+                    return null;
+            }
+
+            var files = new List<string>();
+
+            for (int i = 1; i < args.Length; i++)
+            {
+                string path = args[i];
+
+                if (Directory.Exists(path))
+                {
+                    var folderFiles = Directory.GetFiles(path, "*.vsdx");
+                    files.AddRange(folderFiles);
+
+                    Console.WriteLine("Folder: {0} ({1} files)", path, folderFiles.Length);
+                }
+                else if (File.Exists(path))
+                {
+                    files.Add(path);
+
+                    Console.WriteLine("File: {0}", path);
+                }
+                else
+                {
+                    Console.WriteLine("File or Folder not fount: {0}", path);
+                }
+            }
+
+            Console.WriteLine();
+
+            return files;
+        }
+
+        static void CheckLinks(List<string> files)
+        {
+            Console.WriteLine("Checking links ...");
+            Console.WriteLine();
+
+            var pages = new List<VisioPage>();
+            var links = new List<LinkDetails>();
+
+            Console.WriteLine("Extracting hyperlinks from files ({0})...", files.Count);
+            foreach (var file in files)
+                ExtractLinks(file, pages, links);
+            Console.WriteLine();
+
+            var urls = new List<string>();
+            foreach (var link in links)
+                if (!urls.Contains(link.Url))
+                    urls.Add(link.Url);
+
+            Console.WriteLine("Processing unique hyperlinks ({0}) ...", urls.Count);
+            Parallel.ForEach(urls, new ParallelOptions { MaxDegreeOfParallelism = 8 },
+            url =>
+            {
+                if (!CheckUrl(url))
+                {
+                    Console.WriteLine("Broken: {0}", url);
+
+                    foreach (var link in links.Where(x => String.Equals(x.Url, url, StringComparison.OrdinalIgnoreCase)))
+                        link.Broken = true;
+                }
+            });
+            Console.WriteLine();
+
+            int brokenLinks = links.Where(x => x.Broken).Count();
+            if (brokenLinks == 0)
+            {
+                Console.WriteLine("No broken links found!");
+                return;
+            }
+
+            Console.WriteLine("Broken Links by File and Page ({0}):", brokenLinks);
+            foreach (var file in files)
+                foreach (var link in links.Where(x => x.File == file && x.Broken))
+                    Console.WriteLine("{0}, {1}, {2}, {3}", link.File, GetPageDisplayName(pages, link.File, link.Page), link.Description, link.Url);
+        }
+
+        static void UpdateLinks(List<string> files)
+        {
+            Console.WriteLine("Updating links ...");
+            Console.WriteLine();
+
+            var linkSub = new Dictionary<string, string>();
+
+            using (var streamReader = new StreamReader("LinkUpdates.csv"))
+            {
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    var components = line.Split(',');
+                    linkSub.Add(components[0], components[1]);
+                }
+            }
+
+            foreach (var file in files)
+            {
+                Console.WriteLine("Processing file: {0} ...", file);
+
+                // Open for Read/Write access, we are going to update files in this mode! :)
+                using (var fileStream = new FileStream(file, FileMode.Open, FileAccess.ReadWrite, FileShare.None))
+                using (var zipArchive = new ZipArchive(fileStream, ZipArchiveMode.Update, true))
+                {
+                    foreach (var entry in zipArchive.Entries)
+                    {
+                        if (Path.GetDirectoryName(entry.FullName) == "visio\\pages" && entry.Name != "pages.xml") // page?.xml files
+                        {
+                            using (var entryStream = entry.Open())
+                            {
+                                var xDoc = XDocument.Load(entryStream, LoadOptions.PreserveWhitespace | LoadOptions.SetLineInfo);
+                                var ns = xDoc.Root.GetDefaultNamespace();
+
+                                var linkElements = xDoc.Root.Element(ns + "Shapes")
+                                    .Elements(ns + "Shape")
+                                    .Elements(ns + "Section").Where(x => x.Attribute("N").Value == "Hyperlink")
+                                    .Elements(ns + "Row");
+
+                                bool updated = false;
+                                foreach (var linkElement in linkElements)
+                                {
+                                    var cells = linkElement.Elements(ns + "Cell");
+                                    
+                                    // Look for link updates ...
+                                    string address = cells.First(x => x.Attribute("N").Value == "Address").Attribute("V").Value;
+                                    string extraInfo = cells.First(x => x.Attribute("N").Value == "ExtraInfo").Attribute("V").Value;
+
+                                    string url = address;
+                                    if (!String.IsNullOrWhiteSpace(extraInfo))
+                                        url += "?" + extraInfo;
+
+                                    if (linkSub.ContainsKey(url))
+                                    {
+                                        var newUrlParts = linkSub[url].Split('?');
+
+                                        string newAddress = newUrlParts[0];
+                                        string newExtraInfo = newUrlParts.Length == 2 ? newUrlParts[0] : String.Empty;
+
+                                        cells.First(x => x.Attribute("N").Value == "Address").Attribute("V").Value = newAddress;
+                                        cells.First(x => x.Attribute("N").Value == "ExtraInfo").Attribute("V").Value = newExtraInfo;
+
+                                        updated = true;
+                                    }
+
+                                    // Ensure all links open in a New Window ...
+                                    var newWindowV = cells.First(x => x.Attribute("N").Value == "NewWindow").Attribute("V");
+                                    if (newWindowV.Value == "0")
+                                    {
+                                        newWindowV.Value = "1";
+                                        updated = true;
+                                    }
+                                }
+
+                                if (updated)
+                                {
+                                    Console.WriteLine("Saving file page: {0}", entry.FullName);
+                                    entryStream.Seek(0, SeekOrigin.Begin);
+                                    xDoc.Save(entryStream, SaveOptions.DisableFormatting);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        static void ExtractLinks(string file, List<VisioPage> pages, List<LinkDetails> links)
+        {
+            Console.WriteLine("Processing file: {0} ...", file);
+
+            using (var fileStream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (var zipArchive = new ZipArchive(fileStream, ZipArchiveMode.Read, true))
+            {
+                foreach (var entry in zipArchive.Entries)
+                {
+                    if (Path.GetDirectoryName(entry.FullName) == "visio\\pages" && entry.Name == "pages.xml")
+                    {
+                        // Process the pages.xml to get the display names and rID (rels ID)
+
+                        using (var entryStream = entry.Open())
+                        {
+                            var xDoc = XDocument.Load(entryStream, LoadOptions.PreserveWhitespace);
+
+                            var ns = xDoc.Root.GetDefaultNamespace();
+                            var nsR = xDoc.Root.GetNamespaceOfPrefix("r");
+
+                            var pageElements = xDoc.Root.Elements(ns + "Page");
+                            foreach (var pageElement in pageElements)
+                            {
+                                string displayName = pageElement.Attribute("Name").Value;
+                                
+                                var relElement = pageElement.Element(ns + "Rel");
+                                string rid = relElement.Attribute(nsR + "id").Value;
+
+                                var page = pages.FirstOrDefault(x => x.Id == rid);
+                                if (page == null)
+                                {
+                                    page = new VisioPage() { File = file, Id = rid };
+                                    pages.Add(page);
+                                }
+
+                                page.DisplayName = displayName;
+                            }
+                        }
+                    }
+                    else if (Path.GetDirectoryName(entry.FullName) == "visio\\pages\\_rels" && entry.Name == "pages.xml.rels")
+                    {
+                        // Process the _rels/pages.xml.rels to get the rID to page mapping
+                        // <Relationship Target="page1.xml" Type="http://schemas.microsoft.com/visio/2010/relationships/page" Id="rId1"/>
+
+                        using (var entryStream = entry.Open())
+                        {
+                            var xDoc = XDocument.Load(entryStream, LoadOptions.PreserveWhitespace);
+                            var ns = xDoc.Root.GetDefaultNamespace();
+                            var relationships = xDoc.Root.Elements(ns + "Relationship");
+                            foreach (var relationship in relationships)
+                            {
+                                string target = relationship.Attribute("Target").Value;
+                                string rid = relationship.Attribute("Id").Value;
+
+                                var page = pages.FirstOrDefault(x => x.Id == rid);
+                                if (page == null)
+                                {
+                                    page = new VisioPage() { File = file, Id = rid };
+                                    pages.Add(page);
+                                }
+
+                                page.Page = target;
+                            }
+                        }
+                    }
+                    else if (Path.GetDirectoryName(entry.FullName) == "visio\\pages") // page?.xml
+                    {
+                        using (var entryStream = entry.Open())
+                        {
+                            var xDoc = XDocument.Load(entryStream, LoadOptions.PreserveWhitespace);
+                            var ns = xDoc.Root.GetDefaultNamespace();
+
+                            var linkElements = xDoc.Root.Element(ns + "Shapes")
+                                .Elements(ns + "Shape")
+                                .Elements(ns + "Section").Where(x => x.Attribute("N").Value == "Hyperlink")
+                                .Elements(ns + "Row");
+
+                            foreach (var linkElement in linkElements)
+                            {
+                                var cells = linkElement.Elements(ns + "Cell");
+
+                                string url = cells.First(x => x.Attribute("N").Value == "Address").Attribute("V").Value;
+                                string description = cells.First(x => x.Attribute("N").Value == "Description").Attribute("V").Value;
+                                string extraInfo = cells.First(x => x.Attribute("N").Value == "ExtraInfo").Attribute("V").Value;
+
+                                if (!String.IsNullOrWhiteSpace(extraInfo))
+                                    url += "?" + extraInfo;
+
+                                links.Add(new LinkDetails()
+                                {
+                                    Broken      = false,
+                                    Description = description,
+                                    File        = file,
+                                    Page        = entry.Name,
+                                    Url         = url
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        static bool CheckUrl(string url)
+        {
+            //Console.WriteLine("Checking {0} ...", url);
+            try
+            {
+                var request = WebRequest.Create(url) as HttpWebRequest;
+                request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+                request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0";
+
+                request.Method = "GET";
+                //request.Method = "HEAD";
+
+                using (var response = request.GetResponse() as HttpWebResponse)
+                {
+                    //Console.WriteLine(response.StatusCode);
+                    return (response.StatusCode == HttpStatusCode.OK);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + " - " + url);
+                return false;
+            }
+        }
+
+        static string GetPageDisplayName(List<VisioPage> pages, string file, string page)
+        {
+            var visioPage = pages.FirstOrDefault(x => x.File == file && x.Page == page);
+            if (visioPage == null)
+                return page;
+
+            return visioPage.DisplayName;
+        }
+
+    }
+}
---
+++ SvgFixer.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace VisioSvgFixer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Visio SVG Fixer");
+            Console.WriteLine("by Aaron Dinnage");
+            Console.WriteLine();
+
+            if (args.Length < 1)
+            {
+                Usage();
+                return;
+            }
+
+            var files = ProcessArguments(args);
+            if (files == null || files.Count == 0)
+            {
+                Usage();
+                return;
+            }
+
+            FixFiles(files);
+        }
+
+        static void Usage()
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine("      VisioSvgFixer.exe <file_or_folder_paths>...");
+            Console.WriteLine();
+            Console.WriteLine("All parameters will be considered a file or folder path to be processed.");
+            Console.WriteLine("* Files will be individually processed.");
+            Console.WriteLine("* Folders will have all .SVG files inside them processed (excludes sub-folders).");
+            Console.WriteLine();
+        }
+
+        static List<string> ProcessArguments(string[] args)
+        {
+            var files = new List<string>();
+
+            foreach (string path in args)
+            {
+                if (Directory.Exists(path))
+                {
+                    var folderFiles = Directory.GetFiles(path, "*.svg");
+                    files.AddRange(folderFiles);
+
+                    Console.WriteLine("Folder: {0} ({1} files)", path, folderFiles.Length);
+                }
+                else if (File.Exists(path))
+                {
+                    files.Add(path);
+
+                    Console.WriteLine("File: {0}", path);
+                }
+                else
+                {
+                    Console.WriteLine("File or Folder not fount: {0}", path);
+                }
+            }
+
+            Console.WriteLine();
+
+            return files;
+        }
+
+        static void FixFiles(List<string> files)
+        {
+            foreach (var file in files)
+            {
+                Console.WriteLine("Processing file: {0} ...", file);
+                bool updated = false;
+
+                var xDoc = XDocument.Load(file, LoadOptions.None);
+                var ns = xDoc.Root.GetDefaultNamespace();
+                var xmlNs = xDoc.Root.GetNamespaceOfPrefix("xml");
+
+                // Visio exports an xml:space="preserve" attribute on the <svg> element preventing reformatting to optimise file size
+                var spaceAttribute = xDoc.Root.Attribute(xmlNs + "space");
+                if (spaceAttribute != null && String.Equals(spaceAttribute.Value, "preserve", StringComparison.OrdinalIgnoreCase))
+                {
+                    spaceAttribute.SetValue("default");
+                    updated = true;
+                }
+
+                // Remove all comments
+                var comments = xDoc.DescendantNodes().OfType<XComment>();
+                if (comments.Count() != 0)
+                {
+                    comments.Remove();
+                    updated = true;
+                }
+
+                // Remove all "title" and "desc" nodes.
+                RecursiveRemove(xDoc.Root.Elements(), ns, "title", ref updated);
+                RecursiveRemove(xDoc.Root.Elements(), ns, "desc", ref updated);
+
+                if (updated)
+                {
+                    Console.WriteLine("Saving file: {0}", file);
+                    xDoc.Save(file, SaveOptions.DisableFormatting); // SaveOptions.DisableFormatting removes all whitespace
+                }
+            }
+        }
+
+        static void RecursiveRemove(IEnumerable<XElement> subElements, XNamespace ns, string tag, ref bool updated)
+        {
+            foreach (var subElement in subElements)
+            {
+                var element = subElement.Element(ns + tag);
+                if (element != null)
+                {
+                    element.Remove();
+                    updated = true;
+                }
+
+                RecursiveRemove(subElement.Elements(), ns, tag, ref updated);
+            }
+        }
+
+    }
+}
